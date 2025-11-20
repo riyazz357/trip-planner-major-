75da3f3088415d267334f1fd5d717d9fb47090f1
mohammad riyazuddin <mohammadriyazuddin494@gmail.com>
2025-11-21 00:35:04 +0530

    changed the index file

--- trip-planner-main/server/index.js
+++ trip-planner-main/server/index.js
@@ -68,7 +68,7 @@ app.post("/generate-itinerary", (req, res) => {
 // Serve Frontend
 // app.use(express.static(path.join(__dirname, "../client/build")));
 
-// app.get("*", (req, res) => {
+//app.get("*", (req, res) => {
 //   res.sendFile(path.resolve(__dirname, "../", "client", "build", "index.html"));
 // });
 
