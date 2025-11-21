e8377da4ebba0e1f758c3b2a612e8f2b5b8f0248
mohammad riyazuddin <mohammadriyazuddin494@gmail.com>
2025-11-21 13:48:38 +0530

    force cors headers

--- trip-planner-main/server/app.py
+++ trip-planner-main/server/app.py
@@ -89,6 +89,12 @@ def predict_class(sentence):
 
 # --- 4. Create the /chat API Endpoint ---
 # In app.py, replace your old @app.route('/chat') with this one
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
 
 @app.route('/chat', methods=['POST'])
 def chat():
